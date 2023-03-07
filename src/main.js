62610b149291e68f0e0595fd3cb7ee28d2d9c6e9
Matt Tomasini <matteo.tomasini@protonmail.com>
2023-03-07 11:59:55 +0100

    Set up environment for tutorial

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import {
+    Camera,
+    Group,
+    Scene,
+} from './vendor/three/build/three.module.js';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
