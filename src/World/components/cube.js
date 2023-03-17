3259dd8ab9c15a8208722ef208e0c7bbb9cb9e12
mtomasini <matteo.tomasini@lir.gu.se>
2023-03-17 11:00:20 +0100

    Added texture to cube

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -4,11 +4,20 @@ import {
     Mesh, 
     MeshStandardMaterial,
     MathUtils, 
+    TextureLoader,
 } from '../../../vendor/three/build/three.module.js';
+
+function createMaterial() {
+    const textureLoader = new TextureLoader();
+    const texture = textureLoader.load('/assets/textures/uv-test-bw.png',);
+    const material = new MeshStandardMaterial({ map: texture });
+
+    return material;
+}
 
 function createCube() {
     const geometry = new BoxBufferGeometry(2,2,2);
-    const material = new MeshStandardMaterial({color: 'purple'});
+    const material = createMaterial();
     const cube = new Mesh(geometry, material);
 
     cube.rotation.set(-0.5, -0.1, 0.8)
